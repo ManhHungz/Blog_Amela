5390b6cedb81756de6dfed0f9b05fe5c7558a749
HungzD <daomanhhung3105@gmail.com>
2023-01-31 11:37:08 +0700

    change file web.php

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//datatables
-Route::get('/datatables', function () {
-    return view('datatables');
-});
-
 Auth::routes();
 
 //Page admin
