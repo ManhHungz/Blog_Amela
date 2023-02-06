374e979a491beca51fd466860245d92fbac11f51
Mạnh Hưng <47775334+ManhHungz@users.noreply.github.com>
2023-02-06 07:54:48 +0700

    update api
    edit route category
    edit route product

--- routes/api.php
+++ routes/api.php
@@ -23,13 +23,15 @@ Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'regi
 Route::middleware('auth:api')->group(function () {
     Route::get('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
     Route::post('/refresh', [\App\Http\Controllers\Api\AuthController::class, 'refresh']);
+});
 
-    Route::prefix('customer')->group(function () {
-        Route::get('/categories', [\App\Http\Controllers\Api\CategoryController::class, 'index']);
-        Route::get('/categories/{id}', [\App\Http\Controllers\Api\CategoryController::class, 'detailCategory']);
+Route::prefix('categories')->group(function () {
+        Route::get('/index', [\App\Http\Controllers\Api\CategoryController::class, 'index']);
+        Route::get('/detail/{id}', [\App\Http\Controllers\Api\CategoryController::class, 'detailCategory']);
+});
 
-        Route::get('/products', [\App\Http\Controllers\Api\ProductController::class, 'index']);
-        Route::get('/products/{id}', [\App\Http\Controllers\Api\ProductController::class, 'detailProduct']);
-        Route::get('/products-search/{name}', [\App\Http\Controllers\Api\ProductController::class, 'search']);
-    });
+Route::prefix('products')->group(function () {
+        Route::get('/index', [\App\Http\Controllers\Api\ProductController::class, 'index']);
+        Route::get('/detail/{id}', [\App\Http\Controllers\Api\ProductController::class, 'detailProduct']);
+        Route::get('/search/{name}', [\App\Http\Controllers\Api\ProductController::class, 'search']);
 });
